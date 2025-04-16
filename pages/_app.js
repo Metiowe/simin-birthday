7f049a84305a9872c06f7eb88e661c36bfe6854f
Metiowe <mehdibavi66@gmail.com>
2025-04-16 12:23:26 +0200

    🚀 Ready for deploy: Fonts, Config, Loader Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
-// pages/_app.js
-import "../styles/globals.css"; // oder Alias wenn eingerichtet
+import "../styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Leichtes Fahren</title>
       </Head>
       <Component {...pageProps} />
     </>
